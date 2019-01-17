b8155beaaec91a64af9796574811678b2302c91b
Guilherme Vieira <guilhermevdm@gmail.com>
2019-01-17 10:36:21 -0300

    Added permission policy so that only the owner of the post will have write permissions, anyone else has read-only permissions

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,19 @@
 # posts/views.py
-from rest_framework import generics, permissions
+from rest_framework import generics
+
+from .permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
 
-    # permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    # permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
