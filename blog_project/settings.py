f99118c8eef380eabd30f9cbb50e0ab849f0e72e
Guilherme Vieira <guilhermevdm@gmail.com>
2019-01-19 23:58:35 -0300

    Added token authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd-party apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',  # new
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth',  # new
+    'rest_auth.registration',
 
     # Local
     'posts.apps.PostsConfig',
@@ -48,7 +55,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+
 }
 
 
@@ -131,3 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
